d5c4aaeb5dd30e6e351b49d32ed1e3232636387e
Trivaxy <itsmeomar2003@yahoo.com>
2023-03-11 17:56:44 +0300

    debug

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use logos::Logos;
+use std::path::Path;
 
-use crate::{lexer::Token, parser::Parser, codegen::CodeGenerator};
+use crate::codegen::CodeGenerator;
 
 mod lexer;
 mod parser;
@@ -8,12 +8,20 @@ mod codegen;
 mod types;
 
 fn main() {
-    let test = "fn determinant(a: int, b: int, c: int) -> int { return b * b - 4 * a * c; }";
-    let codegen = CodeGenerator::compile_src(test);
+    let test = "fn determinant(a: int, b: int, c: int) -> int {
+        return b * b - 4 * a * c;
+    }
+    
+    fn main() {
+        determinant(4, 1, 2);
+    }
+    ";
+
+    let codegen = CodeGenerator::compile_src(test, "test");
 
     match codegen {
         Ok(gen) => {
-            println!("{}", gen.get_output());
+            gen.output_to_dir(Path::new("output"));
         },
         Err(errs) => println!("{:?}", errs)
     };
