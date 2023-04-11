2d24502dc0159833cb35f48157a6899499274f5e
Trivaxy <itsmeomar2003@yahoo.com>
2023-04-11 10:10:35 +0300

    optimize tokenizer and handle newlines better

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,7 +1,14 @@
 use logos::{Lexer, Logos};
 
+#[derive(Default)]
+struct LexerExtras {
+    line: usize,
+    last_line_idx: usize,
+}
+
 #[derive(Clone, Logos, Debug, PartialEq)]
-pub enum Token {
+#[logos(extras = LexerExtras)]
+pub enum Token<'a> {
     #[regex(r"[0-9]+", |tok| tok.slice().parse())]
     Number(i32),
 
@@ -101,27 +108,28 @@ pub enum Token {
     #[token("->")]
     Arrow,
 
-    #[regex(r"[a-zA-Z_]+[a-zA-Z0-9_]*", |tok| tok.slice().to_owned())]
-    Identifier(String),
+    #[regex(r"[a-zA-Z_]+[a-zA-Z0-9_]*", |tok| tok.slice())]
+    Identifier(&'a str),
 
     #[regex(r"@[p|r|a|e|s]", |tok| tok.slice().chars().nth(1).unwrap())]
     Selector(char),
 
-    // used internally to keep track of what line we're on
-    // the parser will never receive this from the token stream
-    #[token("\n")]
+    #[token("\n", |lex| {
+        lex.extras.line += 1;
+        lex.extras.last_line_idx = lex.span().end;
+        logos::Skip
+    })]
     Newline,
 
     #[error]
-    #[regex(r"[ \t\f]+", logos::skip)]
+    #[regex(r#"[ \t\f]+"#, logos::skip)]
     Error,
 }
 
 pub struct TokenStream<'a> {
-    lexer: Lexer<'a, Token>,
-    current: Option<Token>,
-    next: Option<Token>,
-    line: usize,
+    lexer: Lexer<'a, Token<'a>>,
+    current: Option<Token<'a>>,
+    next: Option<Token<'a>>,
     col: usize,
 }
 
@@ -133,34 +141,21 @@ impl<'a> TokenStream<'a> {
             lexer,
             current: None,
             next,
-            line: 0,
             col: 0,
         }
     }
 
     pub fn next(&mut self) -> Option<&Token> {
-        self.discard_newlines();
-
         self.current = self.next.take();
         self.next = self.lexer.next();
 
-        self.discard_newlines();
+        self.col = self.lexer.span().start - self.lexer.extras.last_line_idx;
 
-        self.col = self.lexer.span().start;
         self.current.as_ref()
     }
 
     pub fn peek(&mut self) -> Option<&Token> {
-        self.discard_newlines();
         self.next.as_ref()
-    }
-
-    fn discard_newlines(&mut self) {
-        while let Some(Token::Newline) = self.next {
-            self.next = self.lexer.next();
-            self.line += 1;
-            self.col = 0;
-        }
     }
 
     pub fn current(&mut self) -> Option<&Token> {
@@ -168,7 +163,7 @@ impl<'a> TokenStream<'a> {
     }
 
     pub fn line(&self) -> usize {
-        self.line
+        self.lexer.extras.line
     }
 
     pub fn col(&self) -> usize {
