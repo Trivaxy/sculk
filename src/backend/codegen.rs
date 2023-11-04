e594cd0db5ea2e4f796301c40302ba6990cf3ca5
Trivaxy <itsmeomar2003@yahoo.com>
2023-11-04 14:16:30 +0300

    arithmetic IR operations -> commands

--- src/backend/codegen.rs
+++ src/backend/codegen.rs
@@ -1,6 +1,6 @@
-use std::{collections::{HashMap, VecDeque}, default};
+use std::{collections::{HashMap, VecDeque, HashSet}, default};
 
-use crate::data::ResourceLocation;
+use crate::{data::ResourceLocation, parser::Operation};
 
 use super::{ir::{IrFunction, Instruction}, function::FunctionSignature, type_pool::TypePool};
 
@@ -76,7 +76,7 @@ impl IrFunctionCompiler<'_> {
     fn new(ir: &IrFunction, types: TypePool, func_signatures: &HashMap<String, FunctionSignature>) -> Self {
         Self {
             ir,
-            eval_stack: EvaluationStack::new(),
+            eval_stack: EvaluationStack::new(ir.name().to_string()),
             types,
             func_signatures
         }
@@ -85,29 +85,195 @@ impl IrFunctionCompiler<'_> {
 
 #[derive(Default)]
 struct EvaluationStack {
-    slots: VecDeque<i32>, // tmps in scoreboard
+    objective: String,
+    available_slots: VecDeque<i32>,
+    used_slots: HashSet<i32>,
     commands: Vec<CommandAction>,
     stack: Vec<i32>,
 }
 
 impl EvaluationStack {
-    fn new() -> Self {
+    fn new(objective: String) -> Self {
         Self {
+            objective,
             ..Default::default()
         }
     }
 
     fn push_integer(&mut self, n: i32) {
+        let slot = self.find_free_slot();
 
+        self.commands.push(CommandAction::SetScoreboardEntry {
+            entry: self.slot_location(slot),
+            value: GreenValue::Number(n)
+        });
     }
 
-    fn find_free_slot(&mut self) -> i32 {
-        if self.slots.is_empty() {
-            self.slots.push_front(0);
+    fn push_boolean(&mut self, b: bool) {
+        let slot = self.find_free_slot();
+
+        self.commands.push(CommandAction::SetScoreboardEntry {
+            entry: self.slot_location(slot),
+            value: GreenValue::Number(if b { 1 } else { 0 })
+        });
+    }
+
+    fn push_local(&mut self, local: usize) {
+        let slot = self.find_free_slot();
+
+        self.commands.push(CommandAction::SetScoreboardEntry {
+            entry: self.slot_location(slot),
+            value: GreenValue::ScoreboardEntry(self.local_location(local))
+        });
+    }
+
+    fn add(&mut self) {
+        let b = self.stack.pop().unwrap();
+        let a = self.stack.pop().unwrap();
+
+        self.free_slot(b);
+
+        self.commands.push(CommandAction::ScoreboardOperation {
+            op: ScoreboardOperationType::Add,
+            a: self.slot_location(a),
+            b: self.slot_location(b)
+        });
+    }
+
+    fn handle_arithmetic_op(&mut self, op: ScoreboardOperationType) {
+        let b = self.stack.pop().unwrap();
+        let a = self.stack.pop().unwrap();
+
+        self.free_slot(b);
+
+        self.commands.push(CommandAction::ScoreboardOperation {
+            op,
+            a: self.slot_location(a),
+            b: self.slot_location(b),
+        });
+
+        self.stack.push(a);
+    }
+
+    fn handle_comparison_op(&mut self, op: Operation) {
+        let b = self.stack.pop().unwrap();
+        let a = self.stack.pop().unwrap();
+
+        self.free_slot(b);
+
+        self.commands.push(CommandAction::ScoreboardOperation {
+            op: ScoreboardOperationType::Subtract,
+            a: self.slot_location(a),
+            b: self.slot_location(b),
+        });
+
+        let condition = match op {
+            Operation::Equal => format!("score {} matches 0", self.slot_location(a)),
+            Operation::NotEqual => format!("score {} matches 0", self.slot_location(a)),
+            Operation::GreaterThan => format!("score {} matches 1..", self.slot_location(a)),
+            Operation::LessThan => format!("score {} matches ..-1", self.slot_location(a)),
+            Operation::GreaterThanOrEqual => format!("score {} matches 0..", self.slot_location(a)),
+            Operation::LessThanOrEqual => format!("score {} matches ..0", self.slot_location(a)),
+            _ => unreachable!()
+        };
+
+        // NotEqual is the only one that needs to be inverted
+        if op == Operation::NotEqual {
+            self.commands.push(CommandAction::ExecuteUnless {
+                condition,
+                run: Box::new(CommandAction::SetScoreboardEntry {
+                    entry: self.slot_location(a),
+                    value: GreenValue::Number(1)
+                })
+            });
+        } else {
+            self.commands.push(CommandAction::ExecuteIf {
+                condition,
+                run: Box::new(CommandAction::SetScoreboardEntry {
+                    entry: self.slot_location(a),
+                    value: GreenValue::Number(1)
+                })
+            });
+        }
+
+        self.stack.push(a);
+    }
+
+    fn handle_bin_op(&mut self, op: Operation) {
+        match op {
+            Operation::Add => self.handle_arithmetic_op(ScoreboardOperationType::Add),
+            Operation::Subtract => self.handle_arithmetic_op(ScoreboardOperationType::Subtract),
+            Operation::Multiply => self.handle_arithmetic_op(ScoreboardOperationType::Multiply),
+            Operation::Divide => self.handle_arithmetic_op(ScoreboardOperationType::Divide),
+            Operation::Modulo => self.handle_arithmetic_op(ScoreboardOperationType::Modulo),
+            Operation::CheckEquals 
+            | Operation::NotEquals
+            | Operation::GreaterThan
+            | Operation::LessThan
+            | Operation::GreaterThanOrEquals
+            | Operation::LessThanOrEquals => self.handle_comparison_op(op),
+            Operation::Not => {
+                let a = self.stack.pop().unwrap();
+
+                self.commands.push(CommandAction::ExecuteIf {
+                    condition: format!("score {} matches 0", self.slot_location(a)),
+                    run: Box::new(CommandAction::SetScoreboardEntry {
+                        entry: self.slot_location(a),
+                        value: GreenValue::Number(1)
+                    })
+                });
+
+                self.commands.push(CommandAction::ExecuteIf {
+                    condition: format!("score {} matches 1", self.slot_location(a)),
+                    run: Box::new(CommandAction::SetScoreboardEntry {
+                        entry: self.slot_location(a),
+                        value: GreenValue::Number(0)
+                    })
+                });
+
+                self.stack.push(a);
+            },
+            Operation::Negate => {
+                let a = self.stack.pop().unwrap();
+
+                self.commands.push(CommandAction::ScoreboardOperation {
+                    op: ScoreboardOperationType::Multiply,
+                    a: self.slot_location(a),
+                    b: self.const_location(-1),
+                });
+
+                self.stack.push(a);
+            },
         }
     }
 
+    fn find_free_slot(&mut self) -> i32 {
+        match self.available_slots.pop_front() {
+            Some(slot) => slot,
+            None => {
+                let slot = self.used_slots.len() as i32;
+                self.used_slots.insert(slot);
+                slot
+            }
+        }
+    }
 
+    fn free_slot(&mut self, slot: i32) {
+        self.used_slots.remove(&slot);
+        self.available_slots.push_back(slot);
+    }
+
+    fn slot_location(&self, slot: i32) -> ResourceLocation {
+        ResourceLocation::scoreboard(self.objective, format!("s{}", slot))
+    }
+
+    fn local_location(&self, local: usize) -> ResourceLocation {
+        ResourceLocation::scoreboard(self.objective, format!("l{}", local))
+    }
+
+    fn const_location(&self, constant: i32) -> ResourceLocation {
+        ResourceLocation::scoreboard("const".to_string(), format!("c{}", constant))
+    }
 }
 
 struct CompiledFunction {
@@ -129,10 +295,32 @@ enum CommandAction {
     SetScoreboardEntry {
         entry: ResourceLocation,
         value: GreenValue
-    }
+    },
+    ScoreboardOperation {
+        op: ScoreboardOperationType,
+        a: ResourceLocation,
+        b: ResourceLocation,
+    },
+    ExecuteIf {
+        condition: String,
+        run: Box<CommandAction>
+    },
+    ExecuteUnless {
+        condition: String,
+        run: Box<CommandAction>
+    },
 }
 
 enum GreenValue {
     Number(i32),
     ScoreboardEntry(ResourceLocation)
+}
+
+enum ScoreboardOperationType {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Modulo,
+    // add the others when i feel like it
 }
